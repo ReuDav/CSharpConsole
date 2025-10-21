979863786be8fe427b22ee1d0099517f3f5265d4
ReuDav <reucovd@gmail.com>
2025-10-21 12:23:06 +0200

    Added new feature to Program.cs

--- CSharpConsole/Program.cs
+++ CSharpConsole/Program.cs
@@ -6,6 +6,8 @@ namespace CSharpConsole
     {
         static void Main(string[] args)
         {
+            string szoveg = "Új szöveges változó";
+            Console.WriteLine(szoveg);
             Console.WriteLine("Hello World!");
         }
     }
